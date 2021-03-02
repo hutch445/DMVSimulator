7c9cd12aad9f5d1ab99659fcdb2ee71aa9d9c5ea
hutch445 <65507983+hutch445@users.noreply.github.com>
2021-03-02 18:12:45 -0500

    added Print statements and loop to count how many times a number has been called

--- src/DMVSimulator.java
+++ src/DMVSimulator.java
@@ -2,8 +2,17 @@
 public class DMVSimulator {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println("Welcome to the DMV!");
+		int num = (int) (Math.random() * 100);
+		System.out.println("Your number is " + num);
+		int i = 0;
+		while (i < 100)
+		{
+			System.out.println("Number " + i + " please come to the counter");
+			i++;
+		}
+		System.out.println("Number " + num + " please come to the counter");
+		System.out.println("You do not have the require paperwork");
 	}
 
 }
